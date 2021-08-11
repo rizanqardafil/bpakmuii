de2ac1ad7023813075f28106927c5facf5042008
Your Name <17523221@students.uii.ac.id>
2021-08-11 21:13:19 +0700

    tambahkan model galeri, investor, dan tentang

--- bpakmuii_new/app/Models/InvestorModel.php
+++ bpakmuii_new/app/Models/InvestorModel.php
@@ -21,4 +21,50 @@ class InvestorModel extends Model
 
         return $result;
     }
+
+    public function getLaporan()
+    {
+        $builder = $this->db->table('laporan');
+        $builder->select('laporan.id_laporan, laporan.nama_laporan, laporan.slug_laporan, 
+        laporan.path_laporan, COUNT(gambar_laporan.nama_gambar) AS total_gambar');
+        $builder->join('gambar_laporan', 'laporan.id_laporan = gambar_laporan.id_laporan', 'left');
+        $builder->groupBy('laporan.id_laporan');
+
+        $reports = $builder->get()->getResult();
+        $report_images = $this->getGambarLaporan();
+
+        $index = 0;
+        foreach ($reports as $report) {
+            $report->path_laporan !== '' ? $report->status = 'TERSEDIA' : $report->status = 'BELUM TERSEDIA';
+            $report->gambar_path = [];
+            $report->nama_gambar = [];
+
+            $id_laporan = $report->id_laporan;
+
+            while ($index < sizeof($report_images)) {
+                if ($report_images[$index]['id_laporan'] !== $id_laporan) {
+                    break;
+                };
+
+                $report->gambar_path[] = $report_images[$index]['path_gambar'];
+                $report->nama_gambar[] = $report_images[$index]['nama_gambar'];
+                $index++;
+            }
+        }
+
+        return $reports;
+    }
+
+    public function getGambarLaporan()
+    {
+        $builder = $this->db->table('laporan');
+        $builder->select('gambar_laporan.id_laporan, laporan.nama_laporan, laporan.slug_laporan, gambar_laporan.nama_gambar, 
+        gambar_laporan.slug_gambar, gambar_laporan.path_gambar');
+        $builder->join('gambar_laporan', 'laporan.id_laporan = gambar_laporan.id_laporan', 'right');
+        $builder->orderBy('gambar_laporan.id_laporan', 'ASC');
+
+        $results = $builder->get()->getResultArray();
+
+        return $results;
+    }
 }
---
+++ bpakmuii_new/app/Models/TentangModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TentangModel extends Model
+{
+    protected $table = 'sejarah';
+    protected $primaryKey = "id_sejarah";
+    protected $useTimestamps = true;
+    protected $allowedFields = [
+        'nama_konten', 'slug_sejarah', 'isi_sejarah',
+        'isi_logo', 'path_gambar_sejarah', 'path_gambar_logo'
+    ];
+
+    public function getSejarah()
+    {
+        $history = $this->orderBy('id_sejarah', 'ASC')
+            ->findAll(1);
+
+        return $history;
+    }
+
+    public function getVisiMisi()
+    {
+        $builder = $this->db->table('visi_misi');
+        $builder->orderBy('id_visi_misi', 'ASC');
+
+        $result = $builder->get(1)->getResult();
+
+        return $result;
+    }
+}
